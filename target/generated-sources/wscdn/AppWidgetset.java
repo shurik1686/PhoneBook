d85985a66c7de568e2e1944e364951b29cbe9f02
shurik16@ya.ru <shurik16@ya.ru>
2020-10-15 09:16:40 +1000

    junit

---
+++ target/generated-sources/wscdn/AppWidgetset.java
@@ -0,0 +1,26 @@
+import com.vaadin.server.WidgetsetInfo;
+
+/* Addons in this ws:
+ * 8.0.5
+ * OBF
+ * switch:org.vaadin.teemu:3.0.0
+
+ */
+public class AppWidgetset implements WidgetsetInfo {
+
+    @Override
+    public String getWidgetsetName() {
+        return "ws71e7ebb97dcf464374ad7836ef536fd3";
+    }
+
+    @Override
+    public String getWidgetsetUrl() {
+        return "https://ws.vaadin.com/ws71e7ebb97dcf464374ad7836ef536fd3/ws71e7ebb97dcf464374ad7836ef536fd3.nocache.js";
+    }
+
+    @Override
+    public boolean isCdn() {
+        return !true;
+    }
+
+}
